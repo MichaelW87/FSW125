df049e8f4aa91c4bef5849b61a3600ff1bfe023a
Squeezedcoma <michael.wayner@bryanuniversity.edu>
2022-05-20 15:36:13 -0400

    math

---
+++ week-1/math/app.js
@@ -0,0 +1,7 @@
+const output = require ('./math');
+
+
+console.log(output.sum(2,2));
+console.log(output.sub(15,5));
+console.log(output.multiply(12,14));
+console.log(output.divide(120,15));
---
+++ week-1/math/math.js
@@ -0,0 +1,22 @@
+const sum = (a, b) => {
+    return a + b
+};
+
+const sub = (a, b) => {
+    return a - b
+};
+
+const multiply = (a, b) => {
+    return a * b
+};
+
+const divide = (a, b) => {
+    return a / b
+};
+
+module.exports = {
+    sum,
+    sub,
+    multiply,
+    divide
+};
