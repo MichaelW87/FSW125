a2b34064afe6917062e44d2abc7463a7c425eac2
Squeezedcoma <michael.wayner@bryanuniversity.edu>
2022-06-09 21:47:05 -0400

    updated

--- week-4/server.js
+++ week-4/server.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const app = express();
-const intakeRouter = require('./routes/itemsIntake.js');
+const intakeRouter = require('./routes/items.js');
 
 const PORT = 3000;
 
 app.use(express.json());
-app.use('/itemsIntake', intakeRouter);
+app.use('/items', intakeRouter);
 app.listen(PORT, () => {
     console.log(`App started on port: ${PORT}`);
 });
